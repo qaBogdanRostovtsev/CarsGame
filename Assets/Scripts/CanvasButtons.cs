3465ee8704577bc08dfd2485445e459ad4f1e25e
qaBogdanRostovtsev <114915765+qaBogdanRostovtsev@users.noreply.github.com>
2022-10-03 17:05:28 +0600

    Cars

---
+++ Assets/Scripts/CanvasButtons.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CanvasButtons : MonoBehaviour {
+    
+    public Sprite btn, btnPressed;
+    private Image image;
+
+    void Start() {
+        image = GetComponent<Image>();
+    }
+
+    public void PlayGame() {
+        if(PlayerPrefs.GetString("First Game") == "No")
+            StartCoroutine(LoadScene("Game"));
+        else 
+            StartCoroutine(LoadScene("Study"));
+    }
+
+    public void RestartGame() {
+        StartCoroutine(LoadScene("Game"));
+    }
+    
+    public void SetPressedButton() {
+        image.sprite = btnPressed;
+        transform.GetChild(0).localPosition -= new Vector3(0, 5f, 0);
+    }
+    
+    public void SetDefaultButton() {
+        image.sprite = btn;
+        transform.GetChild(0).localPosition += new Vector3(0, 5f, 0);
+    }
+
+    IEnumerator LoadScene(string name) {
+        float fadeTime = Camera.main.GetComponent<Fading>().Fade(1f);
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene(name);
+    }
+
+}
